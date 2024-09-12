b67de358846c00ba77b904fc1fb9523f3fdb452a
Bonnie1005 <144758268+Bonnie1005@users.noreply.github.com>
2024-09-12 16:05:40 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,11 @@
+//week01-2.cpp
+//SOIT106_BASE_002
+# include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << n;
+}
---
+++ week01/week01-3.cpp
@@ -0,0 +1,18 @@
+//week01-3.cpp
+//SOIT106_BASE_006
+# include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << n;
+    cout << "=50*";
+    cout << n / 50;
+    cout << "+5*";
+    cout << n%50 / 5;
+    cout << "+1*";
+    cout << n%5;
+    cout << "\n";
+}
