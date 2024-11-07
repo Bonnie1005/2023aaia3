78c9a7ca174eb70b40a12f5a674614b4a0d772cb
Bonnie1005 <144758268+Bonnie1005@users.noreply.github.com>
2024-11-07 16:30:30 +0800

    Add files via upload

---
+++ week09/week09-2.c
@@ -0,0 +1,12 @@
+// week09-2.c
+// LeetCode 1572. Matrix Diagonal Sum
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+    int M = matSize;
+    int ans = 0;
+    for(int i=0; i<M; i++){
+        ans += mat[i][i];
+        ans += mat[i][M-1-i];
+    }
+    if(M%2==1) ans -= mat[M/2][M/2];
+    return ans;
+}
