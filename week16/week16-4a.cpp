57f5981097a26973dab2cbf10cc1a49bb819e3d1
Bonnie1005 <144758268+Bonnie1005@users.noreply.github.com>
2024-12-26 16:30:51 +0800

    week16

---
+++ week16/week16-4a.cpp
@@ -0,0 +1,11 @@
+// week16-4a.cpp
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==nullptr) return 0;
+
+        int ans1 = maxDepth(root->left);
+        int ans2 = maxDepth(root->right);
+        return max(ans1, ans2)  + 1;
+    }
+};
